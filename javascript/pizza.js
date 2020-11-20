9c451637700d7659d2f5e03f8ba6d2425f806e9c
John <jashby@student.neumont.edu>
2020-11-20 16:16:24 -0700

    Updated Topping Display

--- javascript/pizza.js
+++ javascript/pizza.js
@@ -8,7 +8,7 @@ const pepperoniClass = document.querySelectorAll(".pepperoni");
 const chickenClass = document.querySelectorAll(".chicken");
 const pineappleClass = document.querySelectorAll(".pineapple");
 const beefClass = document.querySelectorAll(".beef");
-const greenPeppersClass = document.querySelectorAll(".green-peppers");
+const greenPeppersClass = document.querySelectorAll(".greenpeppers");
 const jalapenosClass = document.querySelectorAll(".jalapenos");
 const sausageClass = document.querySelectorAll(".sausage");
 const steakClass = document.querySelectorAll(".steak");
@@ -25,6 +25,10 @@ let pizza = {
   rightToppings: [],
 
   fullToppings: [],
+
+  allToppings: [],
+
+  price: 5,
 };
 
 // Size - Changes Pizza value per radio click
@@ -57,11 +61,15 @@ function size(btnNum, textSize) {
   }
 }
 
-//Toppings Buttons
+//Toppings
 
 function toppings(toppingsClass, specificSide, imgSide, pizzaObject) {
   toppingsClass.forEach((item) => {
     item.addEventListener("click", () => {
+      const i = pizzaObject.indexOf(item.classList[0]);
+      if (i > -1) {
+        pizzaObject.splice(i, 1);
+      }
       if (
         item.classList[1] === `${specificSide}` &&
         item.classList[3] !== "active"
@@ -70,19 +78,24 @@ function toppings(toppingsClass, specificSide, imgSide, pizzaObject) {
         item.classList.add("active");
         item.src = `images/${imgSide}active.png`;
         pizzaObject.push(item.classList[0]);
-        filterExtra(pizzaObject);
+        pizza.allToppings.push(item.classList[0]);
       } else if (
         item.classList[1] === `${specificSide}` &&
         item.classList[3] === "active"
       ) {
         defaultButtonSides(toppingsClass);
-        //item.src = `images/${imgSide}circle.png`;
         const index = pizzaObject.indexOf(item.classList[0]);
+        const allIndex = pizza.allToppings.indexOf(item.classList[0]);
         if (index > -1) {
-          pizzaObject.splice(index, 5);
+          pizzaObject.splice(index, 1);
         }
+        if (allIndex > -1) {
+          pizza.allToppings.splice(allIndex, 1);
+        }
+        filterExtra(pizza.allToppings);
         filterExtra(pizzaObject);
       }
+      pizzaToppingDisplayId.innerHTML = updatePizzaDisplay();
     });
   });
 }
@@ -91,7 +104,16 @@ function filterExtra(pizzaObject) {
   let finalToppings = pizzaObject.filter((extra, index) => {
     return pizzaObject.indexOf(extra) === index;
   });
-  console.log(finalToppings);
+}
+
+function filterAllToppings() {
+  let allTops = [];
+  pizza.allToppings.forEach((item) => {
+    if (!allTops.includes(item)) {
+      allTops.push(item);
+    }
+  });
+  pizza.allToppings = allTops;
 }
 
 function defaultButtonSides(toppingsClass) {
@@ -101,6 +123,28 @@ function defaultButtonSides(toppingsClass) {
   toppingsClass[0].src = "images/leftcircle.png";
   toppingsClass[1].src = "images/fullcircle.png";
   toppingsClass[2].src = "images/rightcircle.png";
+}
+
+function displayToppings() {
+  let toppingList = "";
+  pizza.allToppings.forEach((item) => {
+    toppingList += `<li>${item}</li>`;
+  });
+  return toppingList;
+}
+
+function updatePizzaDisplay() {
+  let imgs = "";
+  pizza.fullToppings.forEach((item) => {
+    imgs += `<img src="full-topping-imgs/${item}.png" class="img-fluid toppings" alt="">`;
+  });
+  pizza.leftToppings.forEach((item) => {
+    imgs += `<img src="left-topping-imgs/${item}.png" class="img-fluid toppings" alt="">`;
+  });
+  pizza.rightToppings.forEach((item) => {
+    imgs += `<img src="right-topping-imgs/${item}.png" class="img-fluid toppings" alt="">`;
+  });
+  return imgs;
 }
 
 toppings(pepperoniClass, "left", "left", pizza.leftToppings);
@@ -142,3 +186,8 @@ toppings(tomatoesClass, "right", "right", pizza.rightToppings);
 toppings(cornClass, "left", "left", pizza.leftToppings);
 toppings(cornClass, "mid", "full", pizza.fullToppings);
 toppings(cornClass, "right", "right", pizza.rightToppings);
+
+bodyId.addEventListener("click", () => {
+  filterAllToppings();
+  addedToppingsId.innerHTML = displayToppings();
+});
